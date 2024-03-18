f3acd3eca45ffbb74a646235d6d0e7629375b377
YiChao Ding <jeffding890430@163.com>
2024-03-18 17:29:28 +0800

    Update download_huggingface.py

--- tools_code/download_huggingface.py
+++ tools_code/download_huggingface.py
@@ -1,5 +1,7 @@
 import os
 
+os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+
 # 下载模型
 os.system('huggingface-cli download --resume-download --local-dir-use-symlinks False YaohuiW/LaVie/ --local-dir /root/temp/LaVie/pretrained_models')
 os.system('huggingface-cli download --resume-download --local-dir-use-symlinks False CompVis/stable-diffusion-v1-4 --local-dir /root/temp/LaVie/pretrained_models/stable-diffusion-v1-4')
